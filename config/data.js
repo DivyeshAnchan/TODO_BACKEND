b9550b115e3c8d2f8598ccc5cd274983f2ce35aa
DivyeshAnchan <divyeshanchan@gmail.com>
2023-07-04 23:08:49 +0530

    Bug Fix

--- config/data.js
+++ config/data.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose'
 export const connectdb = () => {
   mongoose
     .connect(process.env.MONGO_URI, { dbname: 'Project1' })
-    .then(() => {
-      console.log('Connected To db')
+    .then((c) => {
+      console.log(`Connected To db ${c.connection.host}`)
     })
     .catch((error) => {
       console.log(error)
